930d69fe67db9560a66659067715b2faf2604868
JasonHJJin <jinjasonhj@gmail.com>
2024-05-30 21:10:30 -0400

    Made some changes for mobile view

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,19 +2,19 @@
    jQuery plugin settings and other scripts
    ========================================================================== */
 
-$(document).ready(function(){
+$(document).ready(function () {
   // Sticky footer
-  var bumpIt = function() {
-      $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
-    },
+  var bumpIt = function () {
+    $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
+  },
     didResize = false;
 
   bumpIt();
 
-  $(window).resize(function() {
+  $(window).resize(function () {
     didResize = true;
   });
-  setInterval(function() {
+  setInterval(function () {
     if (didResize) {
       didResize = false;
       bumpIt();
@@ -26,7 +26,7 @@ $(document).ready(function(){
   // init sticky sidebar
   $(".sticky").Stickyfill();
 
-  var stickySideBar = function(){
+  var stickySideBar = function () {
     const MINIMUM_WIDTH = 1024;
 
     // Adjust if the follow button is shown based upon screen size
@@ -49,23 +49,25 @@ $(document).ready(function(){
       // unfix
       Stickyfill.stop();
       $(".author__urls").hide();
+
     }
   };
 
   stickySideBar();
 
-  $(window).resize(function(){
+  $(window).resize(function () {
     stickySideBar();
   });
 
   // Follow menu drop down
-  $(".author__urls-wrapper button").on("click", function() {
-    $(".author__urls").fadeToggle("fast", function() {});
+  $(".author__urls-wrapper button").on("click", function () {
+    $(".author__urls").fadeToggle("fast", function () { });
     $(".author__urls-wrapper button").toggleClass("open");
   });
 
+
   // init smooth scroll, this needs to be slightly more than then fixed masthead height
-  $("a").smoothScroll({offset: -65});
+  $("a").smoothScroll({ offset: -65 });
 
   // add lightbox class to all image links
   $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
@@ -77,7 +79,7 @@ $(document).ready(function(){
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
     },
     image: {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
@@ -87,7 +89,7 @@ $(document).ready(function(){
     // make it unique to apply your CSS animations just to this exact popup
     mainClass: 'mfp-zoom-in',
     callbacks: {
-      beforeOpen: function() {
+      beforeOpen: function () {
         // just a hack that adds mfp-anim class to markup
         this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
       }
